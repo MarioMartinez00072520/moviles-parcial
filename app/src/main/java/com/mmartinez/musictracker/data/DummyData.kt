68dd6709f7622c2eb7f418a724d7df903e17692c
MarioMartinez00072520 <00072520@uca.edu.sv>
2023-05-23 09:17:56 -0600

    Add: Music Model (Model Tier Commit)

---
+++ app/src/main/java/com/mmartinez/musictracker/data/DummyData.kt
@@ -0,0 +1,14 @@
+package com.mmartinez.musictracker.data
+
+import com.mmartinez.musictracker.data.model.MusicModel
+
+val genre = "Hip Hop"
+val instruments = "Bajo y Teclado"
+
+val genre2 = "Rock N Roll"
+val instruments2 = "Guitarra y Bateria"
+
+var music = mutableListOf(
+    MusicModel(genre, instruments),
+    MusicModel(genre2, instruments2)
+)
---
+++ app/src/main/java/com/mmartinez/musictracker/data/model/MusicModel.kt
@@ -0,0 +1,6 @@
+package com.mmartinez.musictracker.data.model
+
+data class MusicModel(
+    val genre: String,
+    val instruments: String
+)
